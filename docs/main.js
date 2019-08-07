b4b4c029cdd126171b44ad5e56f4bc86ed6f696e
forestowlet3k <53613759+forestowlet3k@users.noreply.github.com>
2019-08-07 20:36:41 +0900

    Add files via upload

---
+++ docs/main.js
@@ -0,0 +1,17 @@
+﻿var okawari_cent = 265;
+var ACTION_TIMES = 3;
+class Action {
+	constructor(a){
+		console.log("ok");
+		this.acterPosition = a;				
+	}
+	get ActerPosition(){
+		return this.acterPosition;
+	}
+	set ActerPosition(num){
+		this.acterPosition = num;
+	}			
+};
+var actionList = [];
+actionList[0] = new Action(9);
+document.write("おかわり：" + okawari_cent/100);
